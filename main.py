66a2544197eab96ec85fd87ae99bc49d729dcddd
thankmoretaderera <ishmaelpastor@gmail.com>
2023-01-16 08:05:41 +0200

    test1

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import time
 
+# Video call price
+price = 0.5
 
-# Initialize variables
-start_time = time.time()
-price_per_minute = 0.05
-total_cost = 0.0
+# User account balance
+balance = 3.0
 
-# Start timer
-while True:
-    elapsed_time = time.time() - start_time
-    elapsed_minutes = int(elapsed_time / 60)
-    cost = elapsed_minutes * price_per_minute
-    total_cost += cost
 
-    print(f'Elapsed Time: {elapsed_minutes} minutes')
-    print(f'Total Cost: {total_cost}')
-    print()
+if balance < price:
+    print("Insufficient funds. Please add more money to your account.")
+else:
+    print("Starting video call...")
+    start_time = time.time()
 
-    time.sleep(60)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    total_cost = price * (elapsed_time / 60)
+    print("Total cost: $" + str(total_cost))
+    print("Elapsed time: " + str(elapsed_time) + " seconds")
